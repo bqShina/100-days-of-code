cea74a06492da35a8d1c09a70dad297005104f93
bqShina <72922289+bqShina@users.noreply.github.com>
2021-06-20 14:40:03 +1000

    Create odd_or_even.py

---
+++ day_13_debug/odd_or_even.py
@@ -0,0 +1,16 @@
+# before debugging
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+
+# after debugging
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
