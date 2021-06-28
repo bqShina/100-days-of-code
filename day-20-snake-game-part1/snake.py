cb8b3348e9c14a52d8fa94db4feb0bdafb407246
bqShina <72922289+bqShina@users.noreply.github.com>
2021-06-28 21:31:01 +1000

    Create snake.py

---
+++ day-20-snake-game-part1/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for n in range(3):
+            snake_part = Turtle("square")
+            snake_part.color("white")
+            snake_part.penup()
+            snake_part.goto(-20 * n, 0)
+            self.segments.append(snake_part)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
